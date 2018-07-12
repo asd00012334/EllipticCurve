1163e81aa70f93edebe5f080912b52faa9762a71
davidlin <aunbdaunbd@gmail.com>
2018-07-12 15:59:52 +0800

    Add operator/

--- src/Element.cpp
+++ src/Element.cpp
@@ -28,8 +28,11 @@ template <typename Int>
 typename Zm<Int>::Element Zm<Int>::Element::operator+(
 	typename Zm<Int>::Element const& r) const 
 {
-	checkMod<Int>(this->type->mod, r.type->mod);
-	Zm::Element ret((val+r.val)%(this->type->mod), type);
+	checkMod<Int>(type->mod, r.type->mod);
+	typename Zm<Int>::Element ret(
+		(val+r.val)%(type->mod), 
+		type
+	);
 	return ret;
 }
 
@@ -37,9 +40,9 @@ template <typename Int>
 typename Zm<Int>::Element Zm<Int>::Element::operator-(
 	typename Zm<Int>::Element const& r) const 
 {
-	checkMod<Int>(this->type->mod, r.type->mod);
-	Zm::Element ret(
-		(val-r.val+this->type->mod)%(this->type->mod), 
+	checkMod<Int>(type->mod, r.type->mod);
+	typename Zm<Int>::Element ret(
+		(val-r.val+type->mod)%(type->mod), 
 		type
 	);
 	return ret;
@@ -49,29 +52,61 @@ template <typename Int>
 typename Zm<Int>::Element Zm<Int>::Element::operator*(
 	typename Zm<Int>::Element const& r) const 
 {
-	checkMod<Int>(this->type->mod, r.type->mod);
-	Zm::Element ret((val*r.val)%(this->type->mod), type);
+	checkMod<Int>(type->mod, r.type->mod);
+	typename Zm<Int>::Element ret(
+		(val*r.val)%(type->mod), 
+		type
+	);
 	return ret;
 }
 
-/*
+// Only implement for integer.
+template <typename Int>
+Int extended_gcd(Int a, Int mod_in) {
+	Int s = 0, pre_s = 1;
+	Int t = 1, pre_t = 0;
+	Int r = a, pre_r = mod_in;
+
+	while (r != 0) {
+		Int q = pre_r / r;
+		Int tmp_r = r, tmp_s = s, tmp_t = t;
+		
+		r = pre_r-(q*r), pre_r = tmp_r;
+		s = pre_s-(q*s), pre_s = tmp_s;
+		t = pre_t-(q*t), pre_t = tmp_t;
+	}
+
+	if (pre_r == 1) {
+		return pre_t;
+	}
+	else {
+		cerr << "Error: inverse not exists!!\n";
+		throw "inverse not exists";
+	}
+}
+
 template <typename Int>
 typename Zm<Int>::Element Zm<Int>::Element::operator/(
-		Zm<Int>::Element const& r) const {
-	checkMod(*this, r);
+	typename Zm<Int>::Element const& r) const 
+{
+	checkMod<Int>(type->mod, r.type->mod);
+	typename Zm<Int>::Element ret(
+		extended_gcd(r.val, type->mod),
+		type
+	);
+	return ret*(*this);
 }
-*/
 
 template <typename Int>
 bool Zm<Int>::Element::operator==(
 	typename Zm<Int>::Element const& r) const 
 {
-	checkMod<Int>(this->type->mod, r.type->mod);
-	return (this->val == r.val);
+	checkMod<Int>(type->mod, r.type->mod);
+	return (val == r.val);
 }
 
 
 template class Zm<int>::Element;
-template class Zm<BigInt>::Element;
+// template class Zm<BigInt>::Element;
 
 }	// End namespace ECC
