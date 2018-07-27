094c67cf005056381b497d34fd9018f0866505eb
asd00012334 <Taylor@DESKTOP-AEEM776.localdomain>
2018-07-27 23:55:12 +0800

    fail to make test/Taylor

---
+++ test/Taylor/test.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#include <gmpxx.h>
+//#include "GF.hpp"
+#include "Zp.hpp"
+
+namespace ECC{
+
+typedef mpz_class bint;
+typedef Zm<bint> zm;
+typedef Zp<bint> zp;
+typedef zm::Element zmElem;
+typedef zp::Element zpElem;
+
+}
+
+using namespace std;
+using namespace ECC;
+
+
+int main(){
+	zp a(23);
+	
+
+}
